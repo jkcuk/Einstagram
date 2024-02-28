df24a2de5fb861686b9cfb70b8f11dc92fe02266
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-02-28 22:56:39 +0000

    ljhg

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from './three'; 
+import * as THREE from 'three'; 
 
-import { GUI } from './three/addons/libs/lil-gui.module.min.js';
-import { OrbitControls } from './three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 let cameraInside, cameraOutside, cameraPosition = 'Inside lookalike sphere', transformation = 'Lorentz', scene, renderer, videoU, videoE, controls, shaderMaterial, geometry, lookalikeSphere, transformationMatrix;
